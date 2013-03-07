4b63455b7becae2d95db0e534004de85ebb177a1
wlan0 <martin_aranciaga@hotmail.com>
2013-03-06 22:44:37 -0300

    added files

---
+++ main.py
@@ -0,0 +1,149 @@
+import pygame, sys, time, random
+import os
+import player
+from player import *
+
+WIDTH  = 610
+HEIGHT = 400
+
+pygame.init()
+screen = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("Zombie flood")
+player_img = pygame.image.load('img_data/iosu_up.png')
+player_rect = player_img.get_rect()
+player  = Player(player_img)
+speed = 15
+level = 1
+a_shot = False
+bala = pygame.rect.Rect((0,0), (5,5))
+
+
+clock = pygame.time.Clock()
+
+numzomb = 10
+zombie_img = pygame.image.load('img_data/zombie.png')
+zombie_rect = zombie_img.get_rect()
+cantzomb = { }
+
+for i in range(0, 99999+1):
+	cantzomb[i] = zombie_img.get_rect()
+	cantzomb[i].left = random.randrange(0,400)
+	cantzomb[i].top  = random.randrange(0,100)
+
+def add_zombies(cantzomb,zombie_img, numzomb):
+	for i in range(0, numzomb):
+		cantzomb[i] = zombie_img.get_rect()
+		cantzomb[i].left = random.randrange(0,400)
+		cantzomb[i].top  = random.randrange(0,100)
+
+def load(name,x,y):
+	try:
+		img = pygame.image.load("img_data/"+str(name))
+		screen.blit(img, (x,y))
+	except:
+		print "Couldn't load: "+str(name)
+
+def handle(what, level, numzomb):
+	if what == "lose":
+		font = pygame.font.Font(None, 80)
+		text = "Game over"
+		render = font.render(text, 1, (255,255,255))
+		screen.fill((255,0,0))
+		screen.blit(render, (150,170))
+	else:
+		font = pygame.font.Font(None, 80)
+		text = "Next stage"
+		render = font.render(text, 1, (255,255,255))
+		screen.fill((10,66,120))
+		screen.blit(render, (150,170))
+
+def main():
+	walk = False
+	a_shot = False
+	numzomb = 10 
+	cont_zomb = 10
+	end = False
+	pygame.mixer.init()
+	level = 1
+	bala = pygame.rect.Rect((0,0), (5,5))
+	zsound = pygame.mixer.Sound("zombies.wav")
+	while not end:
+		vx,vy = 0,0
+
+		for event in pygame.event.get():
+			
+			if event.type == pygame.QUIT:
+				sys.exit(0)
+			keys = pygame.key.get_pressed()
+			if keys[K_ESCAPE]:
+				sys.exit(0)
+			if keys[K_w]:
+				vy = -speed
+				walk = "top"
+			if keys[K_s]:
+				vy = speed
+				walk = "down"
+			if keys[K_a]:
+				vx = -speed
+				walk = "left"
+			if keys[K_d]:
+				vx = speed
+				walk = "right"
+			if keys[K_SPACE]:
+				pygame.mixer.music.load("shot.wav")
+				pygame.mixer.music.play()
+				pos = player.pos()
+				a_shot = True
+				for i in range (0,20):
+					bala = pygame.rect.Rect((0,0), (5,5))
+					bala.left = pos[2] - 10
+					bala.top = pos[1] - 10
+					bala.right = pos[3] - 10
+
+		player.move(vx,vy)
+		background = load("background.png",0,0)
+		vel = { }
+		for i in range(0,numzomb):
+			vel[i] = random.randrange(0,2)
+			screen.blit(zombie_img, cantzomb[i])
+			if player.col(cantzomb[i]):
+				handle('lose', level, numzomb)
+				end = True
+			if bala.colliderect(cantzomb[i]):
+				cantzomb[i].inflate_ip(-99999,-999999)
+				cont_zomb -= 1
+			if cantzomb[i].bottom >= HEIGHT:
+				handle('lose', level, numzomb)
+				end = True
+			if cont_zomb <= 0:
+				handle('win', level, numzomb)
+				level += 1			
+				numzomb += 2
+				add_zombies(cantzomb,zombie_img, numzomb)
+				cont_zomb = numzomb
+			cantzomb[i].move_ip(0, vel[i])
+		if numzomb > 0:	
+			zsound.play()
+
+   		if a_shot:
+			#if keys[K_d]:
+			#	bala.right += 10
+			#if walk == "left":
+			#	bala.left -= 10
+			#if walk == "top":
+				bala.top -= 10
+				pygame.draw.rect(screen, (255,255,255), bala)
+			#if walk == "down":
+			#	bala.top += 10
+
+		kills = numzomb - cont_zomb
+		fps = str(clock.get_time())
+		time = fps.split(".")[0]
+		needed = numzomb
+		player.hud(screen,kills,level,time,needed)
+		player.update(screen,walk)
+		pygame.display.flip()
+		clock.tick(70)
+		pos = player.pos()
+	sys.exit()
+main()
---
+++ player.py
@@ -0,0 +1,52 @@
+import pygame, random
+from pygame.locals import *
+
+class Player(pygame.sprite.Sprite):
+
+	def __init__(self,image):
+		self.image = image
+		self.rect  = self.image.get_rect()
+		self.rect.top,self.rect.left=300,200
+		pygame.key.set_repeat(1, 25)
+	def move(self,vx,vy):
+		if self.rect.left <= 0:
+			self.rect.left = 0
+		if self.rect.right >= 620:
+			self.rect.right = 620
+		if self.rect.top <= 0:
+			self.rect.top = 0
+		if self.rect.bottom >= 400:
+			self.rect.bottom = 400
+		self.rect.move_ip(vx,vy)
+	
+	def update(self,surface,walk):
+		#if walk == "left":
+		#	self.image = pygame.image.load('img_data/iosu_left.png')
+		#if walk == "right":
+		#	self.image = pygame.image.load('img_data/iosu_right.png')
+		#if walk == "top":
+		#	self.image = pygame.image.load('img_data/iosu_up.png')
+		#if walk == "down":
+		#	self.image = pygame.image.load('img_data/iosu_down.png')
+		surface.blit(self.image,self.rect)
+	
+	def col(self,obj):
+		if self.rect.colliderect(obj):
+			return True
+	def pos(self):
+		append = []
+		append.append(self.rect.bottom)
+		append.append(self.rect.top)
+		append.append(self.rect.left)
+		append.append(self.rect.right)	
+		return append
+
+	def hud(self,screen,kills,level,time,needed):
+		font = pygame.font.Font(None, 20)
+		kills = font.render("Kills: "+str(kills)+"/"+str(needed), 1, (255,255,255))
+		level = font.render("Level: "+str(level), 1, (255,255,255))
+		time = font.render("FPS: "+str(time), 1, (255,255,255))
+		screen.blit(kills, (0,0))
+		screen.blit(level, (80,0))
+		screen.blit(time, (140,0))
+		
